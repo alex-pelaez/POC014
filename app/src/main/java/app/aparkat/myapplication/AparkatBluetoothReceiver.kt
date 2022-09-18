6c75631d811d9b8b76421561e65ba77eb8a014b0
alex-pelaez <alex.pelaez@gmail.com>
2022-09-18 11:31:30 +0200

    On Connect/Disconnect make an http request

--- app/src/main/java/app/aparkat/myapplication/AparkatBluetoothReceiver.kt
+++ app/src/main/java/app/aparkat/myapplication/AparkatBluetoothReceiver.kt
@@ -5,11 +5,13 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 import android.util.Log
+import com.github.kittinunf.fuel.Fuel
 
 
-private const val TAG = "MyBroadcastReceiver"
 
-class MyReceiver : BroadcastReceiver() {
+private const val TAG = "AparkatBluetoothBroadcastReceiver"
+
+class AparkatBluetoothReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         StringBuilder().apply {
@@ -18,7 +20,16 @@ class MyReceiver : BroadcastReceiver() {
             toString().also { log ->
                 Log.d(TAG, log)
                 Toast.makeText(context, log, Toast.LENGTH_LONG).show()
+
             }
         }
+        Fuel.get("https://hello-uun4mb7fqq-no.a.run.app")
+            .response { request, response, result ->
+                val (bytes, error) = result
+                if (bytes != null) {
+                    val aux = "[response bytes] ${String(bytes)}".toString()
+                    Log.d(TAG, aux)
+                }
+            }
     }
 }
